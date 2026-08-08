9e1efd8d2011e3319c5b54b40031374b6c7f5ff6
slwnt31 <slwnt8531@gmail.com>
2026-08-08 23:56:59 +0900

    feat: 프로젝트 초기 세팅

---
+++ src/main/java/com/example/suphernova/SupHeRnovaApplication.java
@@ -0,0 +1,13 @@
+package com.example.suphernova;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupHeRnovaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SupHeRnovaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/suphernova/SupHeRnovaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.suphernova;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupHeRnovaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
